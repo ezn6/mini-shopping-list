eb8a8deca326876ae698313a37ad625f38a3d3f9
ezn6 <kcourtney@naver.com>
2022-07-29 11:01:34 +0900

    리팩토링1 : obj특성을 이용하여 filtering 하기

--- src/main.js
+++ src/main.js
@@ -7,9 +7,6 @@ function loadItems() {
 
 function displayItems(items) {
   const container = document.querySelector('.items');
-  //innerHTML은 ``으로 태그들을 묶어준 문자열 이므로
-  //map으로 배열을 만든 후에 모든 태그들을 문자열로 join해주어야한다.
-  //join을 해주지 않으면 []안에 ``으로 묶인 태그들이 있는것이다.
   container.innerHTML = items.map((item) => createHTMLString(item)).join('');
 }
 
@@ -26,17 +23,16 @@ function setEventListeners(items) {
   const logo = document.querySelector('.logo');
   const buttons = document.querySelector('.buttons');
   logo.addEventListener('click', () => displayItems(items));
+
   buttons.addEventListener('click', (event) => {
     const dataset = event.target.dataset;
-    if (dataset.type == null && dataset.color == null) return;
-    if (dataset.type) {
-      console.log(dataset.type);
-      const type = items.filter((item) => item.type === dataset.type);
-      displayItems(type);
-    } else {
-      const color = items.filter((item) => item.color === dataset.color);
-      displayItems(color);
+    const key = dataset.key;
+    const value = dataset.value;
+    if (key == null || value == null) {
+      return;
     }
+    const filtered = items.filter((item) => item[key] === value);
+    displayItems(filtered);
   });
 }
 
